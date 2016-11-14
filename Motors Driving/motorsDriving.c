8a5acb9958a94e85b4db0299ec1118c6ede7bc01
Felix <feliix16@live.fr>
2016-11-14 22:26:38 +0100

    Premier code pour utilisation de la librairie GPIO en c "wiringPi"
    Validé !

--- Motors Driving/motorsDriving.c
+++ Motors Driving/motorsDriving.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#include "motorsDriving.h"
+#include "wiringPi.h"
+
 void hello(void)
 {
 	printf("Hello World !\n");
+	
+	printf("\n--- Test of WiringPi ---\n\n");
+	
+	printf("Setup ...\n");
+	wiringPiSetup();
+	
+	printf("Pin Mode ...\n");
+	pinMode (PIN_LED, OUTPUT);
+	
+	printf("LED ON ...\n");
+	digitalWrite(PIN_LED, HIGH);
+	delay(3000);
+	
+	printf("LED OFF ...\n");
+	digitalWrite(PIN_LED, LOW);
+	
+	printf("\n---------- End ----------\n");
 }
--- Motors Driving/motorsDriving.h
+++ Motors Driving/motorsDriving.h
@@ -1,6 +1,8 @@
 #ifndef _MOTORS_DRIVING_H_
 #define _MOTORS_DRIVING_H_
 
+#define PIN_LED	1
+
 extern void hello(void);
 
 #endif /* _MOTORS_DRIVING_H_ */
