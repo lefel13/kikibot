6204df3d1f1e970f2cab1e39d255fcdf994b22d2
Felix <feliix16@live.fr>
2017-01-08 23:39:00 +0100

    Codage librairie moteurs
    Ecriture des prototypes, codage de la fonction d'initialisation. Code
    non compilé, non validé.

--- Motors Driving/motorsDriving.c
+++ Motors Driving/motorsDriving.c
@@ -14,24 +14,42 @@
 #include "motorsDriving.h"
 #include "wiringPi.h"
 
-void hello(void)
+#define PWM_STOP		0
+#define PWM_FULL		1024
+#define PWM_MID			(PWM_FULL / 2)
+
+void motors_init(void)
 {
-	printf("Hello World !\n");
+	/* 
+	 * Initialisation de la librairie wiringPi pour utilisation du GPIO
+	 * Utilisation de la méthode wiringPiSetupPhys 
+	 * pour utiliser les numéros de broches du connecteur P1
+	 */
+	wiringPiSetupPhys();
 	
-	printf("\n--- Test of WiringPi ---\n\n");
+	/* Configuration des pins */
+	pinMode(PIN_LED, OUTPUT);
+	pinMode(PIN_ENABLE_1, PWM_OUTPUT); 		//Note : à vérifier compatibilité PWM
+	pinMode(PIN_INPUT_1, OUTPUT);
+	pinMode(PIN_INPUT_2, OUTPUT);
+	pinMode(PIN_ENABLE_2, PWM_OUTPUT); 		//Note : à vérifier compatibilité PWM
+	pinMode(PIN_INPUT_3, OUTPUT);
+	pinMode(PIN_INPUT_4, OUTPUT);
 	
-	printf("Setup ...\n");
-	wiringPiSetup();
-	
-	printf("Pin Mode ...\n");
-	pinMode (PIN_LED, OUTPUT);
-	
-	printf("LED ON ...\n");
+	/* Allumage LED pour signaler présence d'activité */
 	digitalWrite(PIN_LED, HIGH);
-	delay(3000);
 	
-	printf("LED OFF ...\n");
+	/* Mise à l'arrêt de toutes les sorties */
+	digitalWrite(PIN_INPUT_1, LOW);
+	digitalWrite(PIN_INPUT_2, LOW);
+	digitalWrite(PIN_INPUT_3, LOW);
+	digitalWrite(PIN_INPUT_4, LOW);
+	pwmWrite(PIN_ENABLE_1, PWM_STOP);
+	pwmWrite(PIN_ENABLE_2, PWM_STOP);
+	
+	/* Temporisation (ms) */
+	delay(500);
+	
+	/* Extinction LED pour signaler fin d'activité */
 	digitalWrite(PIN_LED, LOW);
-	
-	printf("\n---------- End ----------\n");
 }
--- Motors Driving/motorsDriving.h
+++ Motors Driving/motorsDriving.h
@@ -12,8 +12,63 @@
 #ifndef _MOTORS_DRIVING_H_
 #define _MOTORS_DRIVING_H_
 
-#define PIN_LED	1
+/* Pins definition */
+/* Note : identification des broches selon connecteur P1 de la RPI */
+#define PIN_LED			18
+#define PIN_ENABLE_1	4
+#define PIN_INPUT_1		17
+#define PIN_INPUT_2		22
+#define PIN_ENABLE_2	23
+#define PIN_INPUT_3		24
+#define PIN_INPUT_4		25
 
-extern void hello(void);
+
+/**
+ * \fn void motors_init(void)
+ * \brief Fonction d'initialisation des moteurs.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_init(void);
+
+/**
+ * \fn void motors_moveForward(void)
+ * \brief Cette fonction fait avancer le robot.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_moveForward(void);
+
+/**
+ * \fn void motors_moveBack(void)
+ * \brief Cette fonction fait reculer le robot.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_moveBack(void);
+
+/**
+ * \fn void motors_moveRight(void)
+ * \brief Cette fonction fait tourner le robot à droite.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_moveRight(void);
+
+/**
+ * \fn void motors_moveLeft(void)
+ * \brief Cette fonction fait tourner le robot à gauche.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_moveLeft(void);
+
+/**
+ * \fn void motors_stop(void)
+ * \brief Cette fonction arrète le mouvement du robot.
+ * \param Aucun.
+ * \return Aucun.
+ */
+extern void motors_stop(void);
 
 #endif /* _MOTORS_DRIVING_H_ */
